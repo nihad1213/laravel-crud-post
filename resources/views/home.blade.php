47942c400068aa06274da6dc4342b9385ab91bf0
nihad1213 <nihad.nemetli@gmail.com>
2024-08-14 16:31:05 +0400

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="border: 3px solid #000;">
+        <h2>Register</h2>
+        <form action="/register" method="POST">
+            <input type="text" name="" placeholder="Enter Name">
+            <input type="text" placeholder="Enter Mail">
+            <input type="password" name="" placeholder="Password">
+            <button>Register</button>
+        </form>
+    </div>
+</body>
+</html>
